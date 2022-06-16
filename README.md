36cf489a6cea60102d72a45a428e5f4c3ae0a168
Matthew Hons <devwithmatt@gmail.com>
2022-06-16 20:01:02 +0200

    Creating some routing

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Building a project who consist of practicing the most commun functionnalities of React.js
 
+## Main functionnalities : 
+- Filter by continent
+- Filter depends on a many you want to display the country
+- Display all the country cards
 Made solo by me
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic error aut accusantium consectetur porro minima mollitia voluptatum, sed adipisci dolore repellendus facere ducimus iusto ipsa aliquid omnis quas quam! Quos.</p>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente repellendus sit nemo laudantium id et natus adipisci qui fugit temporibus ipsum vitae commodi impedit dignissimos a rerum officiis cupiditate asperiores itaque mollitia minus, ex magni. A minima hic, quae est dolores magnam doloremque amet, cumque unde maiores distinctio consequuntur provident.</p>
+        </div>
+    );
+};
+
+export default About;
