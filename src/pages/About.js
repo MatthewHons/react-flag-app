d0ac693f6523309f9912a24da5e77d9b0474b4de
Matthew Hons <devwithmatt@gmail.com>
2022-06-17 09:31:27 +0200

    modify the about page text

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,9 +9,8 @@ const About = () => {
             <Navigation />
             <h1>A propos</h1>
             <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic error aut accusantium consectetur porro minima mollitia voluptatum, sed adipisci dolore repellendus facere ducimus iusto ipsa aliquid omnis quas quam! Quos.</p>
+            <p>J'ai créé ce site afin d'améliorer mes compétences en React. Avec pour objectif de trouver un stack dans cette technologie</p>
             <br />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente repellendus sit nemo laudantium id et natus adipisci qui fugit temporibus ipsum vitae commodi impedit dignissimos a rerum officiis cupiditate asperiores itaque mollitia minus, ex magni. A minima hic, quae est dolores magnam doloremque amet, cumque unde maiores distinctio consequuntur provident.</p>
         </div>
     );
 };
