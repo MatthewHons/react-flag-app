b7c1feb8bc62e62e0575ed0d99bcc628c9f80c2a
Matthew Hons <devwithmatt@gmail.com>
2022-06-17 08:56:39 +0200

    Adding the filter by continent and sort by population functionnalities

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,6 +7,8 @@ import { useEffect } from 'react';
 const Countries = () => {
     const [data, setData] = useState([]);
     const [rangeValue, setRangeValue] = useState(36);
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+    const [selectedRadio, setSelectedRadio] = useState("");
     //Le useEffect se joue lorsque le composant est appelé
     useEffect(() => {
         axios
@@ -17,11 +19,28 @@ const Countries = () => {
     return (
         <div className='countries'>
             <ul className="radio-container">
-                <input type="range" min="1" max="250" defaultValue={rangeValue}
-                onChange={(e) => setRangeValue(e.target.value)} />
+                <input type="range"
+                    min="1"
+                    max="250"
+                    defaultValue={rangeValue}
+                    onChange={(e) => setRangeValue(e.target.value)} />
+                    {radios.map((continent) => (
+                        <li>
+                            <input 
+                                type="radio"
+                                id={continent}
+                                name="continentRadio"
+                                checked={continent === selectedRadio}
+                                onChange={(e) => setSelectedRadio(e.target.id)} />
+                            <label htmlFor={continent}>{continent}</label>
+                        </li>
+                    ))}
             </ul>
+            {selectedRadio && <button onClick={() => setSelectedRadio("")}>Annuler la recherche</button>}
             <ul>
                 {data
+                    .filter((country) => country.continents[0].includes(selectedRadio))
+                    .sort((a,b) => b.population - a.population)
                     .slice(0,rangeValue)
                     .map((country, index) => (
                     <Card key={index} country={country} />
